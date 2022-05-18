6752af0e904bd2b51c64bda95e84b0cb88e6f28f
marianaCano <60165489+marianaCano@users.noreply.github.com>
2022-05-17 19:50:19 -0500

    menu con javascript

---
+++ main.js
@@ -0,0 +1,33 @@
+secs = [];
+boton = [];
+window.onload = natura;
+
+function natura(){
+    secs[1] = document.getElementById("s1");
+    secs[2] = document.getElementById("s2");
+    secs[3] = document.getElementById("s3");
+    boton[1] = document.getElementById("b_1");
+    boton[2] = document.getElementById("b_2");
+    boton[3] = document.getElementById("b_3");
+}
+
+function quitar(){
+    for( i=1; i< secs.length; i++){
+        secs[i].classList.add("oculta");
+    }
+}
+function desSelect(){
+    for(i in boton){
+        boton[i].classList.remove("selec")
+    }
+}
+function selec(seccion_id){
+    desSelect();
+    boton [seccion_id].classList.add("select");
+}
+function cambio(target){
+    id = target.id.split("_")[1];
+    quitar();
+    selec(id);
+    secs[id].classList.remove("oculta");
+}
